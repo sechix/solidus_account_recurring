93fa2c0d272e20a62b0b6c133700d880010e0961
sechix <sechix@gmail.com>
2017-10-24 16:50:29 +0200

    error 5

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,7 +2,16 @@ Spree::User.class_eval do
   has_many :subscriptions, class_name: "Spree::Subscription", foreign_key: "user_id"
 
   def find_or_create_stripe_customer(token=nil)
-    return api_customer if stripe_customer_id?
+    if stripe_customer_id?
+      unless api_customer.default_source.nil?
+        card = api_customer.sources.create(source: subscription.card_token)
+        api_customer.default_source = card.id
+        api_customer.save
+        return api_customer
+      else
+        return api_customer
+      end
+    end
     customer = if token
       Stripe::Customer.create(description: email, email: email, card: token)
     else
