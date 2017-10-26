51fea0ed026cb30b26303faf38f16df020b95b85
sechix <sechix@gmail.com>
2017-10-26 12:29:36 +0200

    error

--- app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
+++ app/models/concerns/spree/recurring/stripe_recurring/api_handler/subscription_api_handler.rb
@@ -5,7 +5,15 @@ module Spree
         module SubscriptionApiHandler
           def subscribe(subscription)
             raise_invalid_object_error(subscription, Spree::Subscription)
-            customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
+              if subscription.user.stripe_customer_id.nil?
+                customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
+
+              else
+                customer = subscription.user.find_or_create_stripe_customer(subscription.card_token)
+                card = customer.sources.create(source: subscription.card_token)               
+                customer.default_source = card.id
+                customer.save
+              end
             customer.subscriptions.create(plan: subscription.plan.api_plan_id)
 
           end
--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -2,16 +2,7 @@ Spree::User.class_eval do
   has_many :subscriptions, class_name: "Spree::Subscription", foreign_key: "user_id"
 
   def find_or_create_stripe_customer(token=nil)
-    if stripe_customer_id?
-      unless api_customer.default_source.nil?
-        card = api_customer.sources.create(source: token)
-        api_customer.default_source = card.id
-        api_customer.save
-        return api_customer
-      else
-        return api_customer
-      end
-    end
+    return api_customer if stripe_customer_id?
     customer = if token
       Stripe::Customer.create(description: email, email: email, card: token)
     else
