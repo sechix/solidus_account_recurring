1ac21203b77745708e455b1ab935ace0bceeb90c
sechix <sechix@gmail.com>
2018-08-30 19:19:53 +0200

    reporting error

--- app/controllers/spree/admin/subscriptions_controller.rb
+++ app/controllers/spree/admin/subscriptions_controller.rb
@@ -5,7 +5,27 @@ module Spree
       ransack_date_searchable date_col: 'subscribed_at'
 
       def index
-        @search = Spree::Subscription.ransack(params[:q])
+        params[:q] = {} unless params[:q]
+
+        if params[:q][:returndate_gteq].blank?
+          params[:q][:returndate_gteq] = Date.today + 1.days
+        else
+          params[:q][:returndate_gteq] = params[:q][:returndate_gteq].to_date
+        end
+
+        if params[:q][:returndate_lteq].blank?
+          params[:q][:returndate_lteq] = Date.today.end_of_month
+        else
+          params[:q][:returndate_lteq] = params[:q][:returndate_lteq].to_date
+        end
+
+
+
+        if params[:q]
+          @search = Spree::Subscription.ransack(params[:q])
+        else
+          @search = Spree::Subscription.undeleted
+        end
         @subscriptions = @search.result.includes(plan: :recurring).references(plan: :recurring).page(params[:page]).per(15)
         respond_with(@subscriptions)
       end
