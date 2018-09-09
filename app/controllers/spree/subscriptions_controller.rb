1ac6238e9a1dd89927263d7e246b76a82cadcb16
sechix <sechix@gmail.com>
2018-09-09 16:07:13 +0200

    redirect

--- app/controllers/spree/subscriptions_controller.rb
+++ app/controllers/spree/subscriptions_controller.rb
@@ -54,12 +54,12 @@ module Spree
               else
                 flash[:error] = Spree.t(:error)
                 name_plan = ['/recurring/plans/',@plan.id,'/subscriptions/new'].join("");
-                redirect_to name_plan and return
+                redirect_back_or_default(name_plan)
               end
           else
               flash[:error] = Spree.t(:error)
               name_plan = ['/recurring/plans/',@plan.id,'/subscriptions/new'].join("");
-              redirect_to name_plan and return
+              redirect_back_or_default(name_plan)
           end
     end
 
@@ -68,14 +68,14 @@ module Spree
         if  current_spree_user.update_columns(available_plan1: 0, available_plan2: 0, available_plan3: 0,
                                               own_plan1: 0, own_plan2: 0, own_plan3: 0)
 
-            redirect_to '/account', notice: Spree.t(:subscription_canceled)
+             redirect_back_or_default(account_path)
         else
             flash[:error] = Spree.t(:error)
-            redirect_to  '/account' and return
+            redirect_back_or_default(account_path)
         end
       else
         flash[:error] = Spree.t(:error)
-        redirect_to  '/account' and return
+        redirect_back_or_default(account_path)
       end
    
     end
@@ -83,7 +83,7 @@ module Spree
     def update
       if !current_spree_user.month_order_done
         flash[:error] = Spree.t(:no_change_plan_order_done)
-        redirect_to '/account#my_plans' and return
+        redirect_back_or_default(account_path)
       else
         @plan = Spree::Plan.active.where(id: params[:plan_id]).first
         if @subscription = Spree::Subscription.undeleted.where(id: params[:id]).first
@@ -103,22 +103,22 @@ module Spree
                                                               available_plan2: current_spree_user.available_plan2 + @plan.plan2 - previousplan.plan2,
                                                               available_plan3: current_spree_user.available_plan3 + @plan.plan3 - previousplan.plan3)
                             flash[:notice] = Spree.t(:subscription_change)
-                            redirect_to '/account' and return
+                            redirect_back_or_default(account_path)
                         else
                             flash[:error] = Spree.t(:error)
-                            redirect_to '/account' and return
+                            redirect_back_or_default(account_path)
                         end 
                  else
                   flash[:error] = Spree.t(:error)
-                  redirect_to '/account' and return
+                  redirect_back_or_default(account_path)
                  end 
               else
                   flash[:error] = Spree.t(:error)
-                  redirect_to '/account' and return
+                  redirect_back_or_default(account_path)
               end
            else
               flash[:error] = Spree.t(:error)
-              redirect_to '/account' and return
+              redirect_back_or_default(account_path)
           end
         end
     end
